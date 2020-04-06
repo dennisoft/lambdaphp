a08baf3cbf2f9e4f53e1f3b020784fca30251298
Dennis Njunge <njungd@amazon.com>
2020-04-06 18:50:31 +0200

    Removed bugs

--- index.php
+++ index.php
@@ -16,21 +16,20 @@ $client = new SqsClient([
 try {
     $result = $client->receiveMessage(array(
         'AttributeNames' => ['SentTimestamp'],
+        'MaxNumberOfMessages' => 1,
         'MessageAttributeNames' => ['All'],
         'QueueUrl' => $queueUrl, // REQUIRED
         'WaitTimeSeconds' => 0,
     ));
-    $a = $result->get('Messages');
-    for ($x = 0; $x<=10;$x++) {
-        if (!empty($result->get('Messages'))) {
-            var_dump($result->get('Messages')[$x]);
-            $result = $client->deleteMessage([
-                'QueueUrl' => $queueUrl, // REQUIRED
-                'ReceiptHandle' => $result->get('Messages')[$x]['ReceiptHandle'] // REQUIRED
-            ]);
-        } else {
-            echo "No messages in queue. \n";
-        }
+
+    if (!empty($result->get('Messages'))) {
+        var_dump($result->get('Messages')[0]);
+        $result = $client->deleteMessage([
+            'QueueUrl' => $queueUrl, // REQUIRED
+            'ReceiptHandle' => $result->get('Messages')[0]['ReceiptHandle'] // REQUIRED
+        ]);
+    } else {
+        echo "No messages in queue. \n";
     }
 } catch (AwsException $e) {
     // output error message if fails

dd67c783d3e59c9ab933ed59c9c632a0cd56628c
Dennis Njunge <njungd@amazon.com>
2020-04-06 19:14:39 +0200

    Testing latest

--- test.php
+++ test.php
@@ -2,35 +2,38 @@
 
 require 'vendor/autoload.php';
 
-const awsConfig = [
-    'region'  => 'us-west-2',
-    'version' => 'latest'
-  ];
+class SQSQS {
+    const awsConfig = [
+        'region'  => 'us-west-2',
+        'version' => 'latest'
+      ];
 
+     /**
+      * Initialize SQS.
+      */
+      public static function init() {
+        if (!self::$sqs) {
+          $aws = new \Aws\Sdk(self::awsConfig);
+          self::$sqs = $aws->createSQS();
+        }
+      }
 
-  /**
-  * Initialize SQS.
-  */
-  public static function init() {
-    if (!self::$sqs) {
-      $aws = new \Aws\Sdk(self::awsConfig);
-      self::$sqs = $aws->createSQS();
-    }
-  }
+     /**
+      * Delete message after the operation has completed.
+      */
+      public static function delete($queue, $handle) {
+        self::init();
+        $result = self::$sqs->getQueueUrl(['QueueName' => $queue]);
+        $qurl = $result->get('QueueUrl');
 
- /**
-  * Delete message after the operation has completed.
-  */
-  public static function delete($queue, $handle) {
-    self::init();
-    $result = self::$sqs->getQueueUrl(['QueueName' => $queue]);
-    $qurl = $result->get('QueueUrl');
+        self::$sqs->deleteMessage([
+          'QueueUrl'      => $qurl,
+          'ReceiptHandle' => $handle
+        ]);
+      }
+}
 
-    self::$sqs->deleteMessage([
-      'QueueUrl'      => $qurl,
-      'ReceiptHandle' => $handle
-    ]);
-  }
+$response = new SQSQS();
+$response->delete("sqs-tutorial","uiegvdsklvbdv");
+var_dump($response);
 
-  $response = delete("sqs-tutorial","uiegvdsklvbdv");
-  var_dump($response);

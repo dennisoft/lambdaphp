0020bb3703f535f13a28bcb27a53f3bcc54f269f
Dennis Njunge <njungd@amazon.com>
2020-04-06 20:14:41 +0200

    Testing latest

--- test.php
+++ test.php
@@ -5,32 +5,29 @@ require_once 'vendor/autoload.php';
 $sdk = new \Aws\Sdk();
 $sqsClient = $sdk->createSQS(['region' => 'us-west-2', 'version' => '2012-11-05']);
 
-
+ $result = $sqsClient->getQueueUrl(['QueueName' => "sqs-tutorial"]);
+ $qurl = $result->get('QueueUrl');
 
 echo "Sending message\n";
 $sqsClient->sendMessage(array(
-    'QueueUrl' => $queueUrl,
+    'QueueUrl' => $qurl,
     'MessageBody' => 'Hello Home!',
 ));
-
-
 
 echo "Receiving messages\n";
 $result = $sqsClient->receiveMessage([
     'AttributeNames' => ['All'],
     'MaxNumberOfMessages' => 10,
-    'QueueUrl' => $queueUrl,
+    'QueueUrl' => $qurl,
 ]);
 foreach ($result->search('Messages[]') as $message) {
     echo "- Message: {$message['Body']} (Id: {$message['MessageId']})\n";
 }
 
-
-
 echo "Deleting messages\n";
 foreach ($result->search('Messages[]') as $message) {
     $sqsClient->deleteMessage([
-        'QueueUrl' => $queueUrl,
+        'QueueUrl' => $qurl,
         'ReceiptHandle' => $message['ReceiptHandle']
     ]);
     echo "- Deleted: {$message['MessageId']})\n";
